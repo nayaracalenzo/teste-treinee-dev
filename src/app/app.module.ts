766556c8077cb839302b85bd78ebffcaa7a3c02e
nayaracalenzo <queiroz.14@hotmail.com>
2025-06-09 19:29:44 -0300

    fix: npm start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './layout/header/header.component';
+import { AppHeaderComponent } from './app-header.component';
 
 @NgModule({
   declarations: [
-    HeaderComponent,
-    AppComponent
+    AppComponent,
+    AppHeaderComponent
   ],
   imports: [
     BrowserModule,
